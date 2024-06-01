b7f3e138324ebd351d9000fe6834b241515bfd23
Mancopp <mancoppor@gmail.com>
2024-06-01 19:52:58 +0200

    first commit

---
+++ backend/ingenious/src/main/java/pl/uni/opole/ingenious/IngeniousApplication.java
@@ -0,0 +1,13 @@
+package pl.uni.opole.ingenious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngeniousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngeniousApplication.class, args);
+    }
+
+}
---
+++ backend/ingenious/src/test/java/pl/uni/opole/ingenious/IngeniousApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uni.opole.ingenious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngeniousApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
